723451654ca3501f4e1002c521285b3bbf9aac17
Jan Hickiewicz <janhi@tutanota.com>
2023-01-18 14:20:33 +0100

    add global styles and apply it

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { ThemeProvider } from "styled-components";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { theme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>
