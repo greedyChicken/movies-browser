c828a84c7f9c1a3e8ea7504135bc6f454e4c63e5
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-01-23 12:39:00 +0100

    Page Header added

---
+++ src/common/PageHeader/index.js
@@ -0,0 +1,7 @@
+import { Title } from "./styled";
+
+function PageHeader({ title }) {
+  return <Title>{title}</Title>;
+}
+
+export default PageHeader;
---
+++ src/common/PageHeader/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 1.2;
+  color: ${({ theme }) => theme.color.woodsmoke};
+`;
