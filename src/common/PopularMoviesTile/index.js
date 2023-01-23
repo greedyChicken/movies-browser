5b7b7b6be714650af8164f2d6924e50a5a7444f8
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-01-23 13:20:39 +0100

    Popular Moviest Tile styled

--- src/common/PopularMoviesTile/index.js
+++ src/common/PopularMoviesTile/index.js
@@ -1,11 +1,42 @@
-import { MovieTitle, Poster, TileContainer } from "./styled";
+import {
+  Description,
+  MovieTitle,
+  Poster,
+  Rate,
+  Rating,
+  Release,
+  Star,
+  Tag,
+  TagButton,
+  Tags,
+  TileContainer,
+  Votes,
+} from "./styled";
 import image from "../../images/poster.png";
+import star from "../../images/star.svg";
 
-export const PopularMoviesTile = () => {
+export const PopularMoviesTile = ({ tag }) => {
   return (
     <TileContainer>
       <Poster src={image} alt="Movie Poster"></Poster>
-      <MovieTitle>Mulan</MovieTitle>
+      <Description>
+        <MovieTitle>Mulan</MovieTitle>
+        <Release>2020</Release>
+        <Tags>
+          <TagButton>
+            <Tag>{(tag = "Action")}</Tag>{" "}
+          </TagButton>{" "}
+          <TagButton>
+            <Tag>{(tag = "Adventure")}</Tag>{" "}
+          </TagButton>{" "}
+          <TagButton>
+            <Tag>{(tag = "Drama")}</Tag>{" "}
+          </TagButton>
+        </Tags>
+        <Rating>
+          <Star src={star} /> <Rate>7,8</Rate> <Votes>35 votes</Votes>
+        </Rating>
+      </Description>
     </TileContainer>
   );
 };
--- src/common/PopularMoviesTile/styled.js
+++ src/common/PopularMoviesTile/styled.js
@@ -3,7 +3,6 @@ import { borderRadius, boxShadow } from "../../theme";
 
 export const TileContainer = styled.div`
   max-width: 324px;
-  height: 650px;
   background: ${({ theme }) => theme.color.white};
   box-shadow: ${boxShadow};
   border-radius: ${borderRadius};
@@ -21,6 +20,14 @@ export const Poster = styled.img`
   border-radius: ${borderRadius};
 `;
 
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+`;
+
 export const MovieTitle = styled.h2`
   font-weight: 500;
   font-size: 22px;
@@ -33,7 +40,7 @@ export const MovieTitle = styled.h2`
 
   flex: none;
   order: 0;
-  align-self: center;
+  align-self: left;
   flex-grow: 0;
 `;
 
@@ -43,11 +50,101 @@ export const Release = styled.p`
   line-height: 1.5;
   display: flex;
   align-items: center;
+  margin: 8px 16px;
 
   color: ${({ theme }) => theme.color.waterloo};
 
   flex: none;
-  order: 1;
+  order: 0;
   align-self: flex-start;
+  flex-grow: 0;
+`;
+
+export const Tags = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 8px;
+  margin: 0 16px;
+`;
+
+export const TagButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  gap: 10px;
+  height: 36px;
+  border: none;
+  padding: 8px 16px;
+
+  background: ${({ theme }) => theme.color.mystic};
+  border-radius: ${borderRadius};
+
+  flex: none;
+  order: 2;
+  flex-grow: 0;
+`;
+
+export const Tag = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.4;
+  color: ${({ theme }) => theme.color.woodsmoke};
+
+  display: flex;
+  align-items: center;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const Rating = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 12px;
+  margin: 16px;
+`;
+
+export const Star = styled.img`
+  width: 24px;
+  height: 24px;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export const Rate = styled.p`
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.5;
+
+  display: flex;
+  align-items: center;
+
+  color: ${({ theme }) => theme.color.woodsmoke};
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
+`;
+
+export const Votes = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.5;
+
+  display: flex;
+  align-items: center;
+
+  color: ${({ theme }) => theme.color.waterloo};
+
+  flex: none;
+  order: 2;
   flex-grow: 0;
 `;
