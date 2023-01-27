9beb2a877e14c0a335cb27261e31274c6218b82c
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-01-27 09:54:54 +0100

    Margin 0 added to header

--- src/common/PageHeader/styled.js
+++ src/common/PageHeader/styled.js
@@ -7,6 +7,7 @@ export const Title = styled.h1`
   font-size: 36px;
   line-height: 1.2;
   color: ${({ theme }) => theme.color.woodsmoke};
+  margin: 0;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
     font-size: 18px;
