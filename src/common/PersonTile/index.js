88810eb9f7b28f144bcee82d556f07cfa23f2aa7
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-02-01 19:01:06 +0800

    no profile image case implemented in PersonTile

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -6,10 +6,16 @@ import {
   TileContainer,
 } from "./styled";
 
-export const PersonTile = ({ poster, fullName }) => {
+export const PersonTile = ({ profile, profilePath, fullName }) => {
   return (
     <TileContainer>
-      <PersonPhoto src={poster} alt="Person Photo"></PersonPhoto>
+      {profilePath ? (
+        <PersonPhoto src={profile} alt="Person Photo"></PersonPhoto>
+      ) : (
+        <NoProfileImage>
+          <NoProfileImageIcon />
+        </NoProfileImage>
+      )}
       <PersonDetails>
         <PersonName>{fullName}</PersonName>
         <PersonFunction>{""}</PersonFunction>
