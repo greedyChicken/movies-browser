eabf68dfd3fb474f007e1f6e4bf89a380574c849
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-02-02 15:40:49 +0800

    Search component handled, onInputChange function created

--- src/common/Header/Search/index.js
+++ src/common/Header/Search/index.js
@@ -1,8 +1,23 @@
-import { useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
+import searchQueryParamName from "../../searchQueryParamName";
 import { SearchBar, SearchIcon, SearchInput } from "./styled";
 
 export const Search = () => {
   const location = useLocation();
+  const query = new URLSearchParams(location.search).get(searchQueryParamName);
+  const history = useHistory();
+
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    if (target.value.trim() === "") {
+      searchParams.delete(searchQueryParamName);
+    } else {
+      searchParams.set(searchQueryParamName, target.value);
+    }
+
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
 
   return (
     <SearchBar>
@@ -12,6 +27,8 @@ export const Search = () => {
           location.pathname === "/movies" ? "movies..." : "people..."
         }`}
         type="text"
+        value={query || ""}
+        onChange={onInputChange}
       />
     </SearchBar>
   );
