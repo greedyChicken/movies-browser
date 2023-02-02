e69ab24f0f5bc3dfc8824ef60d411551dd45c271
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-02-02 12:21:58 +0800

    cosmetic changes in Loader component

--- src/common/Loader/styled.js
+++ src/common/Loader/styled.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Spinner = styled.div`
-  margin: 0 auto;
+  margin: 120px auto 24px;
   width: 91px;
   height: 91px;
   border: 11px solid ${({ theme }) => theme.color.snuff};
@@ -13,15 +15,23 @@ export const Spinner = styled.div`
   border-right-color: ${({ theme }) => theme.color.black};
   animation: spin 1.2s linear infinite;
 
-  @media (max-width: ${({ theme }) => theme.breakpoint.small}) {
-    height: 35px;
-    width: 35px;
-    border-width: 4px;
-  }
-
   @keyframes spin {
     to {
       transform: rotate(360deg);
     }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    margin: 60px auto 24px;
+    height: 70px;
+    width: 70px;
+    border-width: 8px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
+    margin: 24px auto 24px;
+    height: 35px;
+    width: 35px;
+    border-width: 4px;
   }
 `;
