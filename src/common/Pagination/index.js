9bcdc523983f6984a5d023df1482efa0b819e7c6
Jan Hickiewicz <janhi@tutanota.com>
2023-02-03 13:41:42 +0100

    delete Buttons component and move it back to Pagination component as it seemed to not work when added pagination logic in separated files

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,18 +1,64 @@
-import React from "react";
 import { PageNumber, Pages, StyledPagination } from "./styled";
-import Buttons from "./Buttons";
+import { Button, ButtonText, StyledButtons, Wrapper } from "./styled";
+import { BackwardArrow, ForwardArrow } from "./buttonArrows";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchMovies,
+  selectPage,
+} from "../../features/movies/MoviesListPage/moviesSlice";
 
 const Pagination = () => {
+  const dispatch = useDispatch();
+  const pageNumber = useSelector(selectPage);
+
+  const changePage = (currentPage, backward, toEnd) => {
+    if (toEnd) {
+      backward
+        ? currentPage !== 1 && dispatch(fetchMovies(1))
+        : currentPage !== 500 && dispatch(fetchMovies(500));
+    } else {
+      backward
+        ? currentPage > 1 && dispatch(fetchMovies(currentPage - 1))
+        : currentPage < 500 && dispatch(fetchMovies(currentPage + 1));
+    }
+  };
+
   return (
     <StyledPagination>
-      <Buttons backward={true} />
+      <StyledButtons>
+        <Button backward onClick={() => changePage(pageNumber, true, true)}>
+          <BackwardArrow />
+          <ButtonText>First</ButtonText>
+          <Wrapper>
+            <BackwardArrow />
+          </Wrapper>
+        </Button>
+
+        <Button backward onClick={() => changePage(pageNumber, true, false)}>
+          <BackwardArrow />
+          <ButtonText>Previous</ButtonText>
+        </Button>
+      </StyledButtons>
       <Pages>
         Page
-        <PageNumber>1</PageNumber>
+        <PageNumber>{pageNumber}</PageNumber>
         of
         <PageNumber>500</PageNumber>
       </Pages>
-      <Buttons backward={false} />
+      <StyledButtons>
+        <Button forward onClick={() => changePage(pageNumber, false, false)}>
+          <ButtonText>Next</ButtonText>
+          <ForwardArrow />
+        </Button>
+
+        <Button forward onClick={() => changePage(pageNumber, false, true)}>
+          <ButtonText>Last</ButtonText>
+          <Wrapper>
+            <ForwardArrow />
+          </Wrapper>
+          <ForwardArrow />
+        </Button>
+      </StyledButtons>
     </StyledPagination>
   );
 };
