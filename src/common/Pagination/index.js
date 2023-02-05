5ca19f4eff0a47774ed2a84ad40563582aac511b
Jan Hickiewicz <janhi@tutanota.com>
2023-02-05 16:14:51 +0100

    make Pagination accept parameters for work on different pages

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,32 +1,27 @@
 import { PageNumber, Pages, StyledPagination } from "./styled";
 import { Button, ButtonText, StyledButtons, Wrapper } from "./styled";
 import { BackwardArrow, ForwardArrow } from "./buttonArrows";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchMovies,
-  selectPage,
-} from "../../features/movies/MoviesListPage/moviesSlice";
+import { useDispatch } from "react-redux";
 
-const Pagination = () => {
+const Pagination = ({ fetchFunction, currentPage, lastPage }) => {
   const dispatch = useDispatch();
-  const pageNumber = useSelector(selectPage);
 
-  const changePage = (currentPage, backward, toEnd) => {
+  const changePage = (page, backward, toEnd) => {
     if (toEnd) {
       backward
-        ? currentPage !== 1 && dispatch(fetchMovies(1))
-        : currentPage !== 500 && dispatch(fetchMovies(500));
+        ? page !== 1 && dispatch(fetchFunction(1))
+        : page !== 500 && dispatch(fetchFunction(500));
     } else {
       backward
-        ? currentPage > 1 && dispatch(fetchMovies(currentPage - 1))
-        : currentPage < 500 && dispatch(fetchMovies(currentPage + 1));
+        ? page > 1 && dispatch(fetchFunction(page - 1))
+        : page < 500 && dispatch(fetchFunction(page + 1));
     }
   };
 
   return (
     <StyledPagination>
       <StyledButtons>
-        <Button backward onClick={() => changePage(pageNumber, true, true)}>
+        <Button backward onClick={() => changePage(currentPage, true, true)}>
           <BackwardArrow />
           <ButtonText>First</ButtonText>
           <Wrapper>
@@ -34,24 +29,24 @@ const Pagination = () => {
           </Wrapper>
         </Button>
 
-        <Button backward onClick={() => changePage(pageNumber, true, false)}>
+        <Button backward onClick={() => changePage(currentPage, true, false)}>
           <BackwardArrow />
           <ButtonText>Previous</ButtonText>
         </Button>
       </StyledButtons>
       <Pages>
         Page
-        <PageNumber>{pageNumber}</PageNumber>
+        <PageNumber>{currentPage}</PageNumber>
         of
         <PageNumber>500</PageNumber>
       </Pages>
       <StyledButtons>
-        <Button forward onClick={() => changePage(pageNumber, false, false)}>
+        <Button forward onClick={() => changePage(currentPage, false, false)}>
           <ButtonText>Next</ButtonText>
           <ForwardArrow />
         </Button>
 
-        <Button forward onClick={() => changePage(pageNumber, false, true)}>
+        <Button forward onClick={() => changePage(currentPage, false, true)}>
           <ButtonText>Last</ButtonText>
           <Wrapper>
             <ForwardArrow />
