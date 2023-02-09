bff2a49672975632e1e2e3c491960c7c53f7bca1
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2023-02-09 10:44:57 +0100

    Hover and active tile-link added

--- src/common/TileLink/index.js
+++ src/common/TileLink/index.js
@@ -3,4 +3,15 @@ import styled from "styled-components";
 
 export const TileLink = styled(Link)`
   text-decoration: none;
+  transition: 0.3s;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}) {
+    width: 100%;
+  }
+    &:hover {
+        transform: scale(1.03);
+    }
+    &:active {
+        transform: scale(1.05);
+    }
 `;
