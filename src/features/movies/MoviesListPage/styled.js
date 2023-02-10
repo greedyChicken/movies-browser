cad2fceadfc274f7bc0ebabe0b024df12fabfdd8
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2023-02-10 13:45:28 +0100

    MovieList styled fixing after wrong merge

--- src/features/movies/MoviesListPage/styled.js
+++ src/features/movies/MoviesListPage/styled.js
@@ -7,12 +7,15 @@ export const Layout = styled.div`
   gap: 24px;
   justify-content: center;
 
+  @media (max-width: ${({ theme }) => theme.breakpoint.extraLarge}px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
   @media (max-width: ${({ theme }) => theme.breakpoint.small}px) {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 16px;
-    gap: 16px;
-    margin: 12px auto 32px;
+    grid-template-columns: 1fr;
+    margin: 12px auto 0;
   }
 `;
