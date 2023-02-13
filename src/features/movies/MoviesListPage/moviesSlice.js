6643e1463b8da35fb404ae9f02ae1053a21a82f3
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-02-13 13:48:41 +1100

    page deleted from moviesSlice

--- src/features/movies/MoviesListPage/moviesSlice.js
+++ src/features/movies/MoviesListPage/moviesSlice.js
@@ -6,7 +6,6 @@ const moviesSlice = createSlice({
     movies: [],
     loading: false,
     error: false,
-    page: 1,
     lastPage: 500,
     moviesCount: null,
   },
@@ -14,13 +13,11 @@ const moviesSlice = createSlice({
   reducers: {
     fetchMovies: (state, { payload: { pageNumber } }) => {
       state.loading = true;
-      state.page = pageNumber;
     },
     fetchMoviesSuccess: (state, { payload: movies }) => {
       state.loading = false;
       state.error = false;
       state.movies = movies.results;
-      state.page = movies.page;
       state.lastPage = movies.total_pages < 500 ? movies.total_pages : 500;
       state.moviesCount = movies.total_results;
     },
@@ -29,13 +26,11 @@ const moviesSlice = createSlice({
     },
     fetchMoviesSearchResults: (state, { payload: { page } }) => {
       state.loading = true;
-      state.page = page;
     },
     fetchMoviesSearchResultsSuccess: (state, { payload: results }) => {
       state.loading = false;
       state.error = false;
       state.movies = results.results;
-      state.page = results.page;
       state.lastPage = results.total_pages < 500 ? results.total_pages : 500;
       state.moviesCount = results.total_results;
     },
@@ -53,7 +48,6 @@ export const {
 const selectMoviesState = (state) => state.movies;
 
 export const selectMovies = (state) => selectMoviesState(state).movies;
-export const selectPage = (state) => selectMoviesState(state).page;
 export const selectLastPage = (state) => selectMoviesState(state).lastPage;
 export const selectLoading = (state) => selectMoviesState(state).loading;
 export const selectError = (state) => selectMoviesState(state).error;
