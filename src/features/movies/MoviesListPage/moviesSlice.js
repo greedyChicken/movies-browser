7ef2ab8faab71f0c3c364bda4097b14b3326b790
Jan Hickiewicz <janhi@tutanota.com>
2023-02-14 16:38:54 +0100

    add page to movies and people states

--- src/features/movies/MoviesListPage/moviesSlice.js
+++ src/features/movies/MoviesListPage/moviesSlice.js
@@ -6,6 +6,7 @@ const moviesSlice = createSlice({
     movies: [],
     loading: false,
     error: false,
+    page: 1,
     lastPage: 500,
     moviesCount: null,
   },
@@ -13,11 +14,13 @@ const moviesSlice = createSlice({
   reducers: {
     fetchMovies: (state, { payload: { pageNumber } }) => {
       state.loading = true;
+      state.page = 1;
     },
     fetchMoviesSuccess: (state, { payload: movies }) => {
       state.loading = false;
       state.error = false;
       state.movies = movies.results;
+      state.page = movies.page;
       state.lastPage = movies.total_pages < 500 ? movies.total_pages : 500;
       state.moviesCount = movies.total_results;
     },
@@ -31,6 +34,7 @@ const moviesSlice = createSlice({
       state.loading = false;
       state.error = false;
       state.movies = results.results;
+      state.page = results.page;
       state.lastPage = results.total_pages < 500 ? results.total_pages : 500;
       state.moviesCount = results.total_results;
     },
@@ -48,6 +52,7 @@ export const {
 const selectMoviesState = (state) => state.movies;
 
 export const selectMovies = (state) => selectMoviesState(state).movies;
+export const selectPage = (state) => selectMoviesState(state).page;
 export const selectLastPage = (state) => selectMoviesState(state).lastPage;
 export const selectLoading = (state) => selectMoviesState(state).loading;
 export const selectError = (state) => selectMoviesState(state).error;
--- src/features/people/peopleSlice.js
+++ src/features/people/peopleSlice.js
@@ -6,17 +6,20 @@ const peopleSlice = createSlice({
     people: [],
     loading: false,
     error: false,
+    page: 1,
     lastPage: 500,
     peopleCount: null,
   },
   reducers: {
     fetchPeople: (state, { payload: pageNumber }) => {
       state.loading = true;
+      state.page = 1;
     },
     fetchPeopleSuccess: (state, { payload: people }) => {
       state.loading = false;
       state.error = false;
       state.people = people.results;
+      state.page = people.page;
       state.lastPage = people.total_pages < 500 ? people.total_pages : 500;
       state.peopleCount = people.total_results;
     },
@@ -30,6 +33,7 @@ const peopleSlice = createSlice({
       state.loading = false;
       state.error = false;
       state.people = results.results;
+      state.page = results.page;
       state.lastPage = results.total_pages < 500 ? results.total_pages : 500;
       state.peopleCount = results.total_results;
     },
@@ -49,6 +53,7 @@ const selectPeopleState = (state) => state.people;
 export const selectPeople = (state) => selectPeopleState(state).people;
 export const selectLoading = (state) => selectPeopleState(state).loading;
 export const selectError = (state) => selectPeopleState(state).error;
+export const selectPage = (state) => selectPeopleState(state).page;
 export const selectLastPage = (state) => selectPeopleState(state).lastPage;
 export const selectPeopleCount = (state) =>
   selectPeopleState(state).peopleCount;
